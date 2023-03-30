ac7ee933b6a9551eb54272712877b12e462c0168
Alena Kiseleva <alena.kiseleva@nevosoft.ru>
2023-03-30 19:41:17 +0300

    Add android support

--- Assets/Scripts/UI/InputPlayerController.cs
+++ Assets/Scripts/UI/InputPlayerController.cs
@@ -16,47 +16,89 @@ interface ISwipe
 
     public Element Target { get; protected set; }
 }
-class MouseSwipe : ISwipe 
+
+abstract class Swipe : ISwipe
 {
     public float minDistanceToRegisterSwipe = 0.55f;
-    Vector3 startPos;
-
-    Element target;
 
     bool[] swipeRegistered = new bool[4];
-
     Element ISwipe.Target { get => target; set => target = value; }
 
-    void ISwipe.Update()
-    {
-        for(int i = 0; i < swipeRegistered.Length; i++) {
-            swipeRegistered[i] = false;
-        }
-
-        if (Input.GetMouseButtonDown(0)) {
-            startPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            var mousePos2D = new UnityEngine.Vector2(startPos.x, startPos.y);
-
-            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
-            target = hit.collider ? hit.collider.GetComponent<Element>() : null;
-        } else if(Input.GetMouseButtonUp(0)) {
-            var endPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            var delta = endPos - startPos;
-            if (target != null && delta.sqrMagnitude > minDistanceToRegisterSwipe * minDistanceToRegisterSwipe) {
-                ISwipe.Type type = ISwipe.Type.Right;
-                if ( Mathf.Abs(delta.x) > Mathf.Abs(delta.y)) {
-                    type = delta.x > 0 ? ISwipe.Type.Right : ISwipe.Type.Left;                    
-                } else {
-                    type = delta.y > 0 ? ISwipe.Type.Top : ISwipe.Type.Bottom;
-                }
-                
-                swipeRegistered[(int)type] = true;
-            }            
-        }        
-    }
-    bool ISwipe.GetSwipe(ISwipe.Type type)
+    Element target;
+    public bool GetSwipe(ISwipe.Type type)
     {
         return swipeRegistered[(int)type];
+    }
+
+    abstract public void Update();
+
+    protected void GetElementFromClickPos(Vector2 pos)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero);
+        target = hit.collider ? hit.collider.GetComponent<Element>() : null;
+    }
+
+    protected void ClearRegisteredSwipes()
+    {
+        for (int i = 0; i < swipeRegistered.Length; i++) {
+            swipeRegistered[i] = false;
+        }
+    }
+    protected void RegisterSwipe(Vector3 endPos)
+    {
+        var delta = endPos - target.transform.position;
+        delta.z = 0;
+
+        if (target != null && delta.sqrMagnitude > minDistanceToRegisterSwipe * minDistanceToRegisterSwipe) {
+            ISwipe.Type type = ISwipe.Type.Right;
+            if (Mathf.Abs(delta.x) > Mathf.Abs(delta.y)) {
+                type = delta.x > 0 ? ISwipe.Type.Right : ISwipe.Type.Left;
+            } else {
+                type = delta.y > 0 ? ISwipe.Type.Top : ISwipe.Type.Bottom;
+            }
+
+            swipeRegistered[(int)type] = true;
+        }
+    }
+}
+class MouseSwipe : Swipe
+{
+    override public void Update()
+    {
+        ClearRegisteredSwipes();
+
+        if (Input.GetMouseButtonDown(0)) {
+            var startPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var mousePos2D = new UnityEngine.Vector2(startPos.x, startPos.y);
+
+            GetElementFromClickPos(mousePos2D);
+        } else if(Input.GetMouseButtonUp(0)) {
+            var endPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RegisterSwipe(endPos);       
+        }        
+    }
+
+}
+
+class AndroidSwipe : Swipe
+{    override public void Update()
+    {
+        ClearRegisteredSwipes();
+
+        if (Input.touchCount >= 0) {
+            switch (Input.GetTouch(0).phase) {
+                case TouchPhase.Began: {
+                    var startPos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+                    var pos = new UnityEngine.Vector2(startPos.x, startPos.y);
+
+                    GetElementFromClickPos(pos);
+                    } break;
+                case TouchPhase.Ended: {
+                        var endPos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+                        RegisterSwipe(endPos);
+                    } break;
+            }
+        }
     }
 }
 
@@ -68,6 +110,11 @@ public class InputPlayerController : MonoBehaviour
     bool blocked;
 
     ISwipe swipe = new MouseSwipe();
+
+    private void Start()
+    {
+        swipe = Input.touchSupported ? new AndroidSwipe() : new MouseSwipe();
+    }
 
     void Update()
     {
